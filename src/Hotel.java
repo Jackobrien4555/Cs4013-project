fb99a0a51394854256ea1f36b8c59dc8762b7e7e
Jackobrien4555 <74323041+Jackobrien4555@users.noreply.github.com>
2021-10-28 09:23:45 +0100

    Add files via upload
    Added Hotel class version 1.0 that defines hotel and its info

---
+++ src/Hotel.java
@@ -0,0 +1,15 @@
+package jack;
+/**
+ * The hotel class defines the hotel and its info
+ * @version 1.0
+ * @author Jack O Brien
+ *@since 2021-10-28
+ */
+
+public class Hotel {
+	private String typeHotel;
+
+public String getHotelType(){
+    return typeHotel;
+  }
+}
