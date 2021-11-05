dd7c3fda117f5246dc78b98aed42b9f03fd34b48
Jackobrien4555 <74323041+Jackobrien4555@users.noreply.github.com>
2021-11-05 12:26:02 +0000

    Add files via upload
    class that defines a room

---
+++ src/Room.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class Room {
+	 private int roomNumber;
+	  private String typeOfRoom;
+	  private String amountOfOccupants;
+
+	  public Room(String type, String occupancy, int number) {
+			this.typeOfRoom = type;
+			this.amountOfOccupants = occupancy;
+			this.roomNumber = number;
+		}
+		  
+	  
+}
