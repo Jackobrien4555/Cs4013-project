910eb3b10aa88c712d31332dd1abbffa5b1eb3ad
jackobrien <jackobrien455@gmail.com>
2021-11-08 08:08:50 +0000

    Updated the rooms classes

--- src/typeOfRoom.java
+++ src/typeOfRoom.java
@@ -1,5 +1,10 @@
 import java.util.Arrays;
-
+/**
+ * class typeOfRoom defines room types 
+ * @author Jack O Brien
+ * @since 05/11/2021
+ * 
+ */
 public class typeOfRoom {
 	private String typeOfRoom;
 	private int[] dailyRates;
@@ -10,7 +15,8 @@ public class typeOfRoom {
 	/**
 	 * creates room object with type occupancy and rate
 	 * @param typeOfRoom The room type eg. Deluxe Single
-
+     * @param occuMax the max amount of occupants
+     * @param occuMin the min amount of occupants
 	 * @param dailyRates rates of the room based on day, eg. [0] monday,[1] tuesday etc...
 	 */
 	public typeOfRoom(String typeOfRoom, String occuMin, String occuMax, int[] dailyRates ) {
@@ -18,20 +24,26 @@ public class typeOfRoom {
 		this.occuMin = occuMin;
 	    this.occuMax = occuMax;
 		this.dailyRates = dailyRates;
-	}
+	}	
 
-	/** sets type of room */
-	public void setRoomType(String type){
-		this.typeOfRoom = type;} 
-
-	/** returns room type*/
+	/**
+	 * returns the type of the room
+	 * @return typeOfRoom the type of the room
+	 */
 	public String getRoomType(){
 		return typeOfRoom;} 
-
-	public int numAvailable() {
+/**
+ * returns the number of this type of room available
+ * @return numAvailable returns the number of this type of room available
+ */
+	public int getNumAvailable() {
 		return numAvailable;
 		
 	}
+	/**
+	 * returns string that shows room type with details
+	 *@return string that shows room type with details
+	 */
 	public String toString() {
 		return typeOfRoom + "," + numAvailable + occuMin + "," + occuMax + "," + Arrays.toString(dailyRates);
 	}
