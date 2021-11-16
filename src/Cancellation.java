c2ac1a747f7976251ffd4c7347101da0486e6252
David Walsh <90868348+DavidWalshCode@users.noreply.github.com>
2021-11-16 17:55:49 +0000

    Added javadoc comments

--- src/Cancellation.java
+++ src/Cancellation.java
@@ -9,18 +9,35 @@ public class Cancellation {
     private Reservation reservation;
     private LocalDate cancellationDate;
     private boolean refunded;
-    private int income; // Not 100% sure what I need to do with this yet
+    private int income;
 
+    /**
+     * Constructor for creating a Cancellation object (used when creating cancellation from user input).
+     * @param reservation The reservation that is being cancelled.
+     */
     public Cancellation(Reservation reservation) {
         this.reservation = reservation;
     }
 
+    /**
+     * Constructor for creating a Cancellation object (used when creating a cancellation read from Cancellations.csv file).
+     * @param reservation The reservation that is being cancelled.
+     * @param cancellationDate The date on which the cancellation was made.
+     * @param refunded whether or not the cancellation resulted in a refund.
+     */
     public Cancellation(Reservation reservation, LocalDate cancellationDate, boolean refunded) {
         this.reservation = reservation;
         this.cancellationDate = cancellationDate;
         this.refunded = refunded;
     }
 
+    /**
+     * Constructor for creating a Cancellation object (used when creating a cancellation read from Cancellations.csv file).
+     * @param reservation The reservation that is being cancelled.
+     * @param cancellationDate The date on which the cancellation was made.
+     * @param refunded whether or not the cancellation resulted in a refund.
+     * @param income
+     */
     public Cancellation(Reservation reservation, LocalDate cancellationDate, boolean refunded, int income) {
         this.reservation = reservation;
         this.cancellationDate = cancellationDate;
@@ -28,39 +45,75 @@ public class Cancellation {
         this.income = income;
     }
 
+    /**
+     * This method returns the reservation that belongs to the current cancellation.
+     * @return reservation the reservation that belongs to the cancellation.
+     */
     public Reservation getReservation() {
         return reservation;
     }
 
+    /**
+     * Returns the cancelled date.
+     * @return cancellationDate the date the cancellation was made.
+     */
     public LocalDate getCancellationDate() {
         return cancellationDate;
     }
 
+    /**
+     * Returns whether or not the cancellation resulted in a refund.
+     * @return refunded shows if the cancellation resulted in a refund.
+     */
     public boolean getRefunded() {
         return refunded;
     }
 
+    /**
+     * Returns the total income of the reservation being cancelled.
+     * @return income the total income of the reservation being cancelled.
+     */
     public int getIncome() {
         return income;
     }
 
+    /**
+     * This method sets the reservation.
+     * @param reservation the reservation.
+     */
     public void setReservation(Reservation reservation) {
         this.reservation = reservation;
     }
 
+    /**
+     * This method sets the cancellation date for the reservation.
+     * @param cancellationDate the date that the reservation is cancelled.
+     */
     public void setCancellationDate(LocalDate cancellationDate) {
         this.cancellationDate = cancellationDate;
     }
 
+    /**
+     * This method sets whether the reservation is refunded or not.
+     * @param refunded whether the reservation is refunded or not.
+     */
     public void setRefunded(boolean refunded) {
         this.refunded = refunded;
     }
 
+    /**
+     * This method sets the total income of the reservation.
+     * @param income the total income to be set.
+     */
     public void setIncome(int income) {
         this.income = income;
     }
 
+    /**
+     * This method checks whether a reservation is refundable or not.
+     */
     public boolean isRefundable() {
+        // Need to work on this
         if (reservation.getResType().equalsIgnoreCase("s")/* && cancellationDate < reservation.getCheckInDate()*/) {
             return true;
         }
@@ -68,8 +121,11 @@ public class Cancellation {
         return false;
     }
 
+    /**
+     * Returns a string that represents the data contained by a cancellation and is compatible with a csv file.
+     */
     public String toString() {
-        // Will be making this look nicer but its a start
+        // Will be making this look nicer
         return String.format("Cancellation - Reservation: %s, " +
                 "Cancellation Date: %s, " +
                 "Refunded: %b, " +
