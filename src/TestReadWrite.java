df5609716804e44158f64b89e9cd1fdf9ca995d5
TheCaister <edisoncai2001@gmail.com>
2021-11-23 21:48:48 +0000

    adding list of admins
    Attempting to work with multiple types of users

--- src/TestReadWrite.java
+++ src/TestReadWrite.java
@@ -1,3 +1,10 @@
 public class TestReadWrite {
-    
+    public static void main(String[] args) {
+        Reader reader = new Reader();
+        String readRes = "reservations/random_res.csv";
+        String readCan = "reservations/cancellations.csv";
+        reader.readReservations(readRes);
+        reader.readCancellations(readCan);
+        System.out.println();
+    }
 }
