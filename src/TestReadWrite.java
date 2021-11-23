d70fb10ec94cac6b37fc1bb2876c64e362be7626
DavidWalshCode <20276885@studentmail.ul.ie>
2021-11-23 23:16:12 +0000

    Updated test

--- src/TestReadWrite.java
+++ src/TestReadWrite.java
@@ -1,12 +1,26 @@
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class TestReadWrite {
     public static void main(String[] args) {
         Reader reader = new Reader();
+        Writer writer = new Writer();
+
+        ArrayList<Room> rooms = new ArrayList<Room>();
+        Reservation myRes = new Reservation(20276885, "Walsh", "S", LocalDate.parse("2021-11-23"), LocalDate.parse("2021-11-23"), 1, rooms, 50.0);
+        Cancellation myCan = new Cancellation(myRes);
+
         String readRes = "reservations/random_res.csv";
         String readCan = "reservations/cancellations.csv";
+        String writeRes = "reservations/test_write_res.csv";
+        String writeCan = "reservations/test_write_can.csv";
+
         ArrayList<Reservation> reservations = reader.readReservations(readRes);
         ArrayList<Cancellation> cancellations = reader.readCancellations(readCan);
+
+        writer.writeReservation(writeRes, myRes);
+        writer.writeCancellation(writeCan, myCan);
+
         System.out.println();
     }
 }
