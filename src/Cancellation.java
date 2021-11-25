31ae97084fd2735ace0d596fc9b98d6c38fb94b9
DavidWalshCode <20276885@studentmail.ul.ie>
2021-11-25 14:06:30 +0000

    Updated toString()

--- src/Cancellation.java
+++ src/Cancellation.java
@@ -104,9 +104,8 @@ public class Cancellation {
      * Returns a string that represents the data contained by a cancellation and is compatible with a csv file.
      */
     public String toString() {
-        // Will be making this look nicer
-        return String.format("Cancellation - Reservation: %s, " +
-                "Cancellation Date: %s, " +
-                "Income: %f", reservation, cancellationDate, income);
+        return String.format("%d,%s,%s,%s,%s,%d,%s,%.2f,%s,%.2f\n", reservation.getResNumber(), reservation.getResName(), reservation.getResType(),
+                reservation.getCheckInDate(), reservation.getCheckOutDate(), reservation.getNumberOfRooms(), reservation.getRooms(),
+                reservation.getTotalCost(), cancellationDate, income);
     }
 }
